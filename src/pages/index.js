6374f785efc9ed3e5953a72d6aee5d65fb4bc811
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-12-13 04:06:02 +0530

    Reset template

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+
+      <div className="text-center">
+        <h1 className="text-9xl text-fuchsia-500 font-black">Airbnb Clone</h1>
+      </div>
+    </>
+  );
+}
