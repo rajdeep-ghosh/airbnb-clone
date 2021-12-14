527d9a93c9cb52b0afac1e36b3fd71bcaba9ae43
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-12-14 10:55:09 +0530

    Add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,34 @@
+import Head from "next/head";
+
+function custom404() {
+  return (
+    <>
+      <Head>
+        <title>404 - Page not found</title>
+        <meta name="description" content="Airbnb Clone 404 - Page not found" />
+        <meta name="author" content="Rajdeep Ghosh" />
+      </Head>
+
+      <div className="p-6 sm:p-12 lg:p-20 w-screen h-screen flex flex-col justify-between bg-[#f5f5f5]">
+        <div className="font-mono text-lg tracking-wider">404</div>
+        <div className="flex flex-col space-y-7">
+          <h1 className=" text-3xl sm:text-5xl font-Rubik font-medium">
+            Page not found
+          </h1>
+          <p className="text-lg">
+            The page you are looking for doesn&apos;t exist or has been moved.
+          </p>
+          <span>
+            Back to{" "}
+            <a className="bg-yellow-400" href="/">
+              home
+            </a>
+          </span>
+        </div>
+        <div></div>
+      </div>
+    </>
+  );
+}
+
+export default custom404;
