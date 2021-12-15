8b4f44883af866ead49eb89400ff38a5a414f8cf
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-12-15 16:32:34 +0530

    Add logo to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,33 @@
+import Image from "next/image";
+import airbnbLogo from "../assets/img/airbnb-logo.svg";
+import airbnbIcon from "../assets/img/airbnb-icon.svg";
+
 function Header() {
-  return <header>This is header</header>;
+  return (
+    <header className="sticky top-0 p-3 md:px-5 grid grid-cols-3 z-50 bg-white shadow-md">
+      {/* Left */}
+      <div className="relative h-10 cursor-pointer my-auto">
+        <Image
+          src={airbnbLogo}
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+          className="[display:none_!important] md:[display:block_!important]"
+        />
+        <Image
+          src={airbnbIcon}
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+          className="md:[display:none_!important]"
+        />
+      </div>
+
+      {/* Middle */}
+
+      {/* Right */}
+    </header>
+  );
 }
 
 export default Header;
