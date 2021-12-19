4c261bc59d6e62f0f18cbdd851a1640498e7680b
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-12-19 06:12:41 +0530

    Add google fonts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -15,6 +15,9 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
         <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@200;300;400;600;700;800;900&display=swap" rel="stylesheet" />
       </Head>
 
       <Component {...pageProps} />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import Header from "../components/Header";
 
 export default function Home() {
