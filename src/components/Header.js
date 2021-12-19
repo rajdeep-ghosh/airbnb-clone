dbaf5f713c935a53bcc4aa26690c8d3996216980
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-12-19 06:14:47 +0530

    Add search bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import airbnbLogo from "../assets/img/airbnb-logo.svg";
 import airbnbIcon from "../assets/img/airbnb-icon.svg";
+import { SearchIcon } from "@heroicons/react/outline";
 
 function Header() {
   return (
-    <header className="sticky top-0 p-3 md:px-5 grid grid-cols-3 z-50 bg-white shadow-md">
+    <header className="sticky top-0 p-3 md:px-5 grid grid-cols-3 z-50 bg-white !font-Nunito-Sans shadow-md">
       {/* Left */}
       <div className="relative h-10 cursor-pointer my-auto">
         <Image
@@ -24,6 +25,12 @@ function Header() {
       </div>
 
       {/* Middle */}
+      <div className="flex justify-between items-center p-2 border-2 rounded-full">
+        <input className="flex-grow" type="text" placeholder="Where are you going?" />
+        <button className="search__btn">
+          <SearchIcon className="h-6 inline" /> <span>Search</span>
+        </button>
+      </div>
 
       {/* Right */}
     </header>
