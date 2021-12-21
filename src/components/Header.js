4ecdef69b2e6b148e2182082172531c80ddb0e3c
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-12-21 05:37:27 +0530

    Complete search bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,30 +5,36 @@ import { SearchIcon } from "@heroicons/react/outline";
 
 function Header() {
   return (
-    <header className="sticky top-0 p-3 md:px-5 grid grid-cols-3 z-50 bg-white !font-Nunito-Sans shadow-md">
+    <header 
+      className="sticky top-0 flex justify-between items-center p-3 md:px-5 z-50 bg-white !font-Nunito-Sans shadow-md"
+    >
       {/* Left */}
       <div className="relative h-10 cursor-pointer my-auto">
         <Image
           src={airbnbLogo}
-          layout="fill"
+          width="105px"
+          height="50px"
           objectFit="contain"
-          objectPosition="left"
           className="!hidden md:!block"
         />
         <Image
           src={airbnbIcon}
           layout="fill"
           objectFit="contain"
-          objectPosition="left"
           className="md:!hidden"
         />
       </div>
 
       {/* Middle */}
-      <div className="flex justify-between items-center p-2 border-2 rounded-full">
-        <input className="flex-grow" type="text" placeholder="Where are you going?" />
+      <div className="flex justify-between items-center p-2 border-2 rounded-full sm:shadow-sm">
+        <input
+          className="flex-1 px-5 text-lg bg-transparent outline-none"
+          type="text"
+          placeholder="Where are you going?"
+        />
         <button className="btn bg-airbnb_red text-white ring-airbnb_red">
-          <SearchIcon className="h-6 inline" /> <span>Search</span>
+          <SearchIcon className="h-6 inline" />{" "}
+          <span className="hidden md:inline">Search</span>
         </button>
       </div>
 
