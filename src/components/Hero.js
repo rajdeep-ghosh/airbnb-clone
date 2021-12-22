5b06545a6a62f5d6b600c33f17da436b2d2f35f0
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-12-22 21:20:16 +0530

    Merge Header and Hero and make them mobile responsive

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,11 @@
 function Hero() {
   return (
-    <section className="bg-[url('../assets/img/hero.jpg')] bg-top bg-cover h-fit pt-60 px-12 pb-24 font-Nunito-Sans">
-      <div className="max-w-[40%] space-y-6">
+    <section className="bg-[url('../assets/img/hero.jpg')] bg-top bg-cover h-fit pt-36 sm:pt-60 pb-48 px-6 sm:px-14 xl:px-40 !font-Nunito-Sans">
+      <div className="max-w-[40%] space-y-10 sm:space-y-6">
         <h1 className="text-[clamp(2rem,_5.5vw,_3.25rem)] leading-[1.2] font-black text-white capitalize">
           shine like the whole universe is yours
         </h1>
-        <button className="btn w-40 h-[3.25rem] py-3 px-6 text-xl font-semibold bg-white text-indigo-900 ring-white ring-offset-indigo-900 whitespace-nowrap capitalize">
+        <button className="btn sm:w-40 sm:h-[3.25rem] py-2 sm:py-3 px-4 sm:px-6 sm:text-xl font-semibold bg-white text-indigo-900 ring-white ring-offset-indigo-900 whitespace-nowrap capitalize">
           explore now
         </button>
       </div>
