3adc2aa608be5cd8cd545d517d7b5fb9831bb54d
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-12-23 17:21:20 +0530

    Add heading to card

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,5 +1,9 @@
 function Cards() {
-  return <section>this is card</section>;
+  return (
+    <section className="mx-6 sm:mx-14 xl:mx-40 mt-12 !font-Nunito-Sans">
+      <h1 className="text-3xl font-extrabold">Live anywhere</h1>
+    </section>
+  );
 }
 
 export default Cards;
