3d8d17eea0bda7d7a3bae31479b84956cb5aff89
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-12-25 06:43:32 +0530

    Finish Discover section

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,10 +4,10 @@ function Cards({ section, content }) {
   return (
     <section className="mx-6 sm:mx-14 xl:mx-40 mt-12 !font-Nunito-Sans selection:text-white">
       <h1 className="text-3xl font-extrabold">{content.heading}</h1>
-      <div className="py-7 flex space-x-4 lg:justify-between overflow-scroll snap-x scrollbar-hide">
+      <div className="py-7 flex space-x-4 lg:justify-between overflow-scroll snap-x scrollbar-hide -mb-6">
         {content.items.map((item, idx) => (
           <div key={idx} className="group snap-center cursor-pointer">
-            <div className="relative h-56 lg:h-[17rem] w-56 lg:w-[17rem] drop-shadow-2xl rounded-xl">
+            <div className={`relative ${section == "live" ? `h-56 w-56 lg:h-[17rem] lg:w-[17rem]` : `h-72 w-72 lg:h-96 lg:w-96`} drop-shadow-2xl rounded-xl`}>
               <Image
                 src={`/images/${item.img}`}
                 layout="fill"
@@ -16,7 +16,7 @@ function Cards({ section, content }) {
                 className="rounded-xl group-hover:scale-95 transition-all duration-200"
               />
             </div>
-            <h3 className="mt-3 text-xl md:text-2xl font-bold">{item.title}</h3>
+            <h3 className="mt-5 text-xl md:text-2xl font-bold">{item.title}</h3>
           </div>
         ))}
       </div>
