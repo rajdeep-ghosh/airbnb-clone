bba1cac19e6f172500c1d642b1b540399022a33c
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2022-01-03 02:12:35 +0530

    Create SearchResults component

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,9 +1,14 @@
 import Header from "../components/Header";
+import SearchResults from "../components/SearchResults";
 
 function Search() {
   return (
     <>
       <Header page="search" />
+
+      <main className="mt-24">
+        <SearchResults />
+      </main>
     </>
   );
 }
