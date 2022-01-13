0250fbbccfd8c6a48e9d8746d8c7c22e40bd0854
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2022-01-13 14:51:27 +0530

    Refactor

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,6 @@
 function Hero() {
   return (
-    <section className="bg-[url('/images/hero.jpg')] bg-top bg-cover h-fit pt-36 sm:pt-60 pb-48 px-6 sm:px-14 xl:px-40 !font-Nunito-Sans">
+    <div className="bg-[url('/images/hero.jpg')] bg-top bg-cover h-fit pt-36 sm:pt-60 pb-48 px-6 sm:px-14 xl:px-40 !font-Nunito-Sans">
       <div className="max-w-[40%] space-y-10 sm:space-y-6">
         <h1 className="text-[clamp(2rem,_5.5vw,_3.25rem)] leading-[1.2] font-black text-white capitalize">
           shine like the whole universe is yours
@@ -9,7 +9,7 @@ function Hero() {
           explore now
         </button>
       </div>
-    </section>
+    </div>
   );
 }
 
