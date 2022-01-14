2e19d800484414bea35f4f78e41c599e3a85f900
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2022-01-14 22:13:28 +0530

    Display checkin data

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,9 +1,4 @@
-import { useRouter } from "next/router";
-
-function SearchResults() {
-  const router = useRouter();
-  const { location, chechInDate, checkOutDate, noOfGuests } = router.query;
-
+function SearchResults({ location, checkInDate, checkOutDate, noOfGuests }) {
   const floatingButton = (text) => (
     <p className="px-4 py-2 border rounded-full cursor-pointer hover:shadow-lg active:scale-90 active:bg-gray-50 transition-transform duration-200 select-none">
       {text}
@@ -12,7 +7,9 @@ function SearchResults() {
 
   return (
     <section className="flex-grow">
-      <p className="text-sm">300+ stays for {noOfGuests} people</p>
+      <p className="text-sm text-gray-500/80">
+        300+ stays ‒ {checkInDate} to {checkOutDate} ‒ {noOfGuests} guest
+      </p>
       <h1 className="mt-3 mb-6 text-3xl font-semibold">
         Stays in <span className="capitalize">{location}</span>
       </h1>
--- src/pages/search.js
+++ src/pages/search.js
@@ -1,13 +1,29 @@
+import { useRouter } from "next/router";
+import { format, parseISO } from "date-fns";
 import Header from "../components/Header";
 import SearchResults from "../components/SearchResults";
 
 function Search() {
+  const router = useRouter();
+
+  const { location, checkInDate, checkOutDate, noOfGuests } = router.query;
+  const formattedCheckInDate = format(parseISO(checkInDate), "dd MMM yy");
+  const formattedCheckoutDate = format(parseISO(checkOutDate), "dd MMM yy");
+
   return (
     <>
-      <Header page="search" />
+      <Header
+        page="search"
+        placeholder={`${location} | ${formattedCheckInDate} ‒ ${formattedCheckoutDate} | ${noOfGuests} guest`}
+      />
 
       <main className="mt-32 mx-6 sm:mx-14 xl:mx-40 flex">
-        <SearchResults />
+        <SearchResults
+          location={location}
+          noOfGuests={noOfGuests}
+          checkInDate={formattedCheckInDate}
+          checkOutDate={formattedCheckoutDate}
+        />
       </main>
     </>
   );
