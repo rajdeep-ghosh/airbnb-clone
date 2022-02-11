5637c9d06d427a8aa81f47f444d4d08261f08ba2
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2022-02-12 00:09:26 +0530

    Get destination id

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,11 +1,12 @@
 import { useRouter } from "next/router";
+import axios from "axios";
 import { format, parseISO } from "date-fns";
 import Header from "../components/Header";
 import SearchResults from "../components/SearchResults";
 import Map from "../components/Map";
 import Footer from "../components/Footer";
 
-function Search({ searchData }) {
+export default function Search({ searchData }) {
   const router = useRouter();
 
   const { location, checkInDate, checkOutDate, noOfGuests } = router.query;
@@ -35,9 +36,29 @@ function Search({ searchData }) {
   );
 }
 
-export default Search;
+export async function getServerSideProps(context) {
+  const { location } = context.query;
 
-export async function getServerSideProps() {
+  // Get Destination ID of location
+  const destinationIdOptions = {
+    method: "GET",
+    url: "https://hotels-com-provider.p.rapidapi.com/v1/destinations/search",
+    params: {
+      query: location,
+      currency: "INR",
+      locale: "en_IN",
+    },
+    headers: {
+      "x-rapidapi-host": "hotels-com-provider.p.rapidapi.com",
+      "x-rapidapi-key": process.env.RAPIDAPI_KEY,
+    },
+  };
+
+  const destinationId = await axios
+    .request(destinationIdOptions)
+    .then((response) => response.data.suggestions[0].entities[0].destinationId)
+    .catch((error) => console.error(error));
+
   const searchData = await fetch("https://jsonkeeper.com/b/5NPS").then((res) =>
     res.json()
   );
